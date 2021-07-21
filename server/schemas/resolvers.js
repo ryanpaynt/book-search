47904b938643858e14726f46bee05f23b1c29842
Ryan <ryanpaynt@uncc.edu>
2021-07-21 09:40:15 -0400

    updated resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,8 +40,35 @@ const resolvers = {
 
       const token = signToken(user);
       return {token, user};
+    },
+
+    saveBook:  async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id},
+          { $addToSet: { savedBooks: args.input } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findAndUpdate(
+          { _id: context.user._id},
+          { $pull: { savedBooks: { bookId: args.bookId} } },
+          { new: true }
+        );
+
+        return updatedUser;
     }
-  },
+
+    throw new AuthenticationError('You need to be logged in!');
+  }
+}
 };
 
 module.exports = resolvers;
